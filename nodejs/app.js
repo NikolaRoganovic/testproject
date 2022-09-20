1d37893b0d6a8a62f835a5a8fea82298adcefa1b
NikolaRoganovic <dzonixd@gmail.com>
2022-09-20 14:37:33 +0200

    bump

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,8 +1,12 @@
-var express = require('express');
-var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello Worlds!');
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  const msg = 'Hello node master!\n'
+  res.end(msg);
 });
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}/`);
 });
